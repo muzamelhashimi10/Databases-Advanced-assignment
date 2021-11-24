337efcc2e7738042603b7e8a4445e832fe827b6e
muzamelhashimi10 <72615624+muzamelhashimi10@users.noreply.github.com>
2021-11-24 19:06:56 +0100

    Improving my code by using redis as caching then sending from redis to mongodb

--- Scraper.py
+++ Scraper.py
@@ -1,11 +1,21 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-from time import time, sleep
+import pymongo
+import redis
+import time
 
-f =  open('bitcoin.txt', 'w+')
-index = 0
-while (index < 10):
+r = redis.Redis()
+
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["Bitcoin"]
+# mycol = mydb["Transactions"]
+
+wait = time.sleep
+
+# f =  open('bitcoin.txt', 'w+')
+
+def scraper():
     # The Code
     url = requests.get(f'https://www.blockchain.com/btc/unconfirmed-transactions')
     soup = BeautifulSoup(url.text, 'html.parser')
@@ -33,19 +43,40 @@ while (index < 10):
         items_list3.append(i)
 
     sorted_list = sorted(items_list3, key=lambda x: float(x[2]),reverse=True)
+
+    Hash = sorted_list[0][0]
+    Time = sorted_list[0][1]
+    BTC = sorted_list[0][2]
+    USD = sorted_list[0][4]
+
+    # Print on the txt file
+    # with open('bitcoin.txt', 'a+') as f:
+
+    # f.write('Hash: '+ item[0] + "\n")
+    # f.write('Time: ' + item[1] + "\n")
+    # f.write('Amount(BTC): ' + item[2] + "\n")
+    # f.write('Amount(USD): ' + item[4] + "\n\n")
+
+    # Print on the console
+    print('Hash: ' + Hash)
+    print('Time: ' + Time)
+    print('Amount(BTC): ' + BTC)
+    print('Amount(USD): ' + USD + "\n")
+
+    # MONGO
+    # mydict = {"Hash": item[0], "Time": item[1], "Amount(BTC)": item[2], "Amount(USD)": item[4]}
+    # x = mycol.insert_one(mydict)
+
+    # REDIS
+    r.hset(Hash, "Time", Time)
+    r.hset(Hash, "BTC", BTC)
+    r.hset(Hash, "USD", USD)
+
+    wait(60)
+
+
+index = 1
+while index < 20:
+    scraper()
     index = index + 1
-    with open('bitcoin.txt', 'a+') as f:
-        for item in sorted_list[:1]:
-            f.write('Hash: '+ item[0] + "\n")
-            print('Hash: '+ item[0])
-            f.write('Time: ' + item[1] + "\n")
-            print('Time: ' + item[1])
-            f.write('Amount(BTC): ' + item[2] + "\n")
-            print('Amount(BTC): ' + item[2])
-            f.write('Amount(USD): ' + item[4] + "\n\n")
-            print('Amount(USD): ' + item[4] + "\n")
-
-    sleep(60 - time() % 60)
-
-
 
